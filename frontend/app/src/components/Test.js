369c9aae7e6077b3dcc40703e928f96dbe9d5962
Zaray <cor191025@uvg.edu.gt>
2021-03-21 15:49:55 -0600

    report

--- frontend/app/src/components/Test.js
+++ frontend/app/src/components/Test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import SideBar from "./sideBar/sideBar";
+import Report from "./report/report";
 
 
 export default class Test extends React.Component {
@@ -9,8 +10,7 @@ export default class Test extends React.Component {
             <div className={'d-flex'}>
                 <SideBar/>
                 <main>
-                    <h1>Hello World</h1>
-                    <Button variant="primary" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Hola Mundo</Button>
+                    <Report />
                 </main>
 
             </div>
---
+++ frontend/app/src/components/report/report.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReportItem from './reportItem'
+
+export default class report extends React.Component{
+    render() {
+        return (
+            <div className={'container-fluid mt-5'}>
+                <h1 className={'text-secondary'}>Reporte</h1>
+                <div className={'d-flex flex-wrap justify-content-around'}>
+                    <ReportItem
+                        title={'Álbumes'}
+                        description={'Álbumes recientes de la semana'}/>
+                    <ReportItem
+                        title={'Artistas'}
+                        description={'Artistas con popularidad creciente en los últimos 3 meses'}/>
+                    <ReportItem
+                        title={'Artistas'}
+                        description={'Artistas con mayor producción musical'}/>
+                    <ReportItem
+                        title={'Géneros'}
+                        description={'Géneros más populares'}/>
+                    <ReportItem
+                        title={'Suscripciones'}
+                        description={'Nuevas suscripciones en los últimos seis meses'}/>
+                    <ReportItem
+                        title={'Usuarios'}
+                        description={'Usuarios más activos'}/>
+                </div>
+            </div>
+
+        );
+    }
+}
