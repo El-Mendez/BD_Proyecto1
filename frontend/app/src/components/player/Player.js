66ce885d599006d6faa754bc6dbce6cab8d81ae9
Pablo Méndez <pablomendez778@gmail.com>
2021-03-21 19:31:00 -0600

    El player no está tan feo ahora

--- frontend/app/src/components/player/Player.js
+++ frontend/app/src/components/player/Player.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Button from "react-bootstrap/Button";
 import './player.scss'
 import YTPlayer from 'yt-player'
-import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa'
+import { FaPauseCircle, FaPlayCircle, FaVolumeUp } from 'react-icons/fa'
 
 export default class Player extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isPaused: true
+            isPaused: true,
+            volume: 100,
         }
 
         this.handlePlay = this.handlePlay.bind(this);
+        this.handleVolumeChange = this.handleVolumeChange.bind(this);
     }
 
     componentDidMount() {
@@ -21,6 +23,7 @@ export default class Player extends React.Component {
             autoplay: true,
             controls: false,
             keyboard: false,
+            volume: this.state.volume
         });
 
         this.player.load(this.props.videoId);
@@ -38,21 +41,40 @@ export default class Player extends React.Component {
         } catch (e) {}
     }
 
+    handleVolumeChange(e) {
+        let volume = e.target.value
+        this.setState({volume: volume})
+        this.player.setVolume(volume);
+    }
 
     render() {
         let item;
         if (this.state.isPaused) {
-            item = <FaPlayCircle />
+            item = <FaPlayCircle size={42} className="colored-icon"/>
         } else {
-            item = <FaPauseCircle />
+            item = <FaPauseCircle size={42} className="colored-icon"/>
         }
 
         return (
-            <div>PLAYER
-                <Button onClick={this.handlePlay}>
-                    {item}
-                </Button>
-                <div id='yt-player'/>
+            <div id="player">
+                <div>
+                    Canción
+                </div>
+                <div>
+                    <Button id="playButton" onClick={this.handlePlay}>
+                        {item}
+                    </Button>
+                    <div id='yt-player'/>
+                </div>
+                <div id="volume-container">
+                    <FaVolumeUp size={20} className="colored-icon" />
+                    <input
+                        type="range"
+                        min="0" max="100"
+                        defaultValue={this.state.volume}
+                        id="volume-slider"
+                        onChange={this.handleVolumeChange}/>
+                </div>
             </div>
         );
     }
