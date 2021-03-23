5665e4b8a188c37d17d55ee2d5ff29c7ca0fa987
Zaray <cor191025@uvg.edu.gt>
2021-03-23 16:16:23 -0600

    navegación al fin funciona

--- frontend/app/src/components/utils/button.js
+++ frontend/app/src/components/utils/button.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default class button extends React.Component{
     render(){
         return(
-            <button  className={`btn ${this.props.btnClass} my-4` }>
+            <button
+                onClick={this.props.handleClick}
+                className={`btn ${this.props.btnClass} my-3`}>
                 {this.props.btnName}
             </button>
         )
