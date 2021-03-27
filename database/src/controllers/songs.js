7ecacee8c9886385645c8913e83ba5eb5c44c16c
Pablo Méndez <pablomendez778@gmail.com>
2021-03-27 00:51:12 -0600

    Rehice el código de manera más simple e incluso usaré eslint solo porque puedo

--- database/src/controllers/songs.js
+++ database/src/controllers/songs.js
@@ -1,13 +1,13 @@
 const pool = require('../credentials');
 
 const getSongs = async (req, res) => {
-    const response = await pool.query(`
+  const response = await pool.query(`
         select c.id_cancion, link, a.id_artista, c.nombre as cancion_nombre, a.nombre as artista_nombre from canciones c
            inner join artista a on a.id_artista = c.id_artista;
-    `)
-    res.status(200).json(response.rows);
+    `);
+  res.status(200).json(response.rows);
 };
 
 module.exports = {
-    getSongs,
-}
+  getSongs,
+};
--- database/src/routes/index.js
+++ database/src/routes/index.js
@@ -1,31 +1,31 @@
 const { Router } = require('express');
+
 const router = Router();
 
-const { createUser, logIn } = require('../controllers/usuarios')
+const { createUser, logIn } = require('../controllers/usuarios');
 const { getSongs } = require('../controllers/songs');
-const { searchArtist } = require('../controllers/artist');
-const { getAlbums } = require('../controllers/albums');
-const { searchSubscription } = require('../controllers/subscriptionTiers')
-const { getSubscription } = require('../controllers/subscription')
-const { getGenres } = require('../controllers/genres')
-const { getPlaylist } = require('../controllers/playlist')
+// const { searchArtist } = require('../controllers/artist');
+// const { getAlbums } = require('../controllers/albums');
+// const { searchSubscription } = require('../controllers/subscriptionTiers')
+// const { getSubscription } = require('../controllers/subscription')
+// const { getGenres } = require('../controllers/genres')
+// const { getPlaylist } = require('../controllers/playlist')
 
 // router.get('/tiers/', searchSubscription);
 
-// router.post('/users/', createUser);
-// router.get('/users/', logIn);
+router.post('/createUser', createUser);
+router.get('/login', logIn);
 
-// router.get('/subscription/', getSubscription);
+// router.get('/subscription', getSubscription);
 
-// router.get('/artist/', searchArtist);
+// router.get('/artist', searchArtist);
 
-// router.get('/genres/', getGenres)
+// router.get('/genres', getGenres)
 
 // router.get('/albums', getAlbums);
 
 router.get('/songs', getSongs);
 
 // router.get('/playlist', getPlaylist)
-
 
 module.exports = router;
