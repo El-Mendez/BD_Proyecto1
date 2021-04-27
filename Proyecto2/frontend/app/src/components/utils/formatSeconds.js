8e7e62648b1941829398474d403af5f328a97044
Orlando <orlando.osberto@gmail.com>
2021-04-27 11:24:25 -0600

    Revert "Moviendo las carpetas para el proyecto 2"
    This reverts commit 853578e7cc31cb6436fc84259c21af7457a7124e.

--- Proyecto2/frontend/app/src/components/utils/formatSeconds.js
+++
@@ -1,6 +0,0 @@
-export default (seconds) => {
-    const minutes = Math.trunc(seconds / 60).toString();
-    seconds = "0" + Math.trunc(seconds % 60).toString();
-
-    return `${minutes}:${seconds.slice(-2)}`
-}
