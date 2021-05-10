f5be626bd14ce86569d4b1acca48aa1467177cfe
Zaray <cor191025@uvg.edu.gt>
2021-05-09 22:47:10 -0600

    monitores y nuevos reportes

--- frontend/app/src/components/report/report.js
+++ frontend/app/src/components/report/report.js
@@ -13,49 +13,112 @@ import BArtistsReport from './reports/bartistsReport';
 import MonthsReport from './reports/newSubsReport';
 import GenresReport from './reports/genresReport';
 import UsersReport from './reports/usersReport';
+import RangeDate from './reports/rangeDate';
+import ModalArtist from './reports/modalArtist';
+import ModalSongsArtists from './reports/modalSongsartists';
 
 
 export default function Report(){
   let {path, url} = useRouteMatch();
+  const [modalShow, setModalShow] = React.useState(false);
+  const [modal1Show, setModal1Show] = React.useState(false);
+  const [modal2Show, setModal2Show] = React.useState(false);
+  const [modal3Show, setModal3Show] = React.useState(false);
 
   return (
     <div className={'overflow-auto'}>
       <div className={'container-fluid mt-2'}>
+        {/* Opciones de reporte */}
         <h1 className={'text-secondary'}>Reportes</h1>
-        <div className={'d-flex flex-wrap justify-content-around'}>
+        <div className={'d-flex flex-wrap'}>
           <ReportItem
             image={albums}
             title={'Álbumes'}
             description={'Álbumes recientes de la semana'}
             redirect={`${url}/recentAlbums`}
+            click={true}
           />
           <ReportItem
             image={famousArtists}
             title={'Artistas'}
             description={'Artistas con popularidad creciente en los últimos 3 meses'}
-            redirect={`${url}/famousArtists`}/>
+            redirect={`${url}/famousArtists`}
+            click={true}
+          />
           <ReportItem
             image={biggerArtists}
             title={'Artistas'}
             description={'Artistas con mayor producción musical'}
-            redirect={`${url}/biggerArtists`}/>
+            redirect={`${url}/biggerArtists`}
+            click={true}
+          />
           <ReportItem
             image={genres}
             title={'Géneros'}
             description={'Géneros más populares'}
-            redirect={`${url}/popularGenres`}/>
+            redirect={`${url}/popularGenres`}
+            click={true}
+          />
           <ReportItem
             image={subscriptions}
             title={'Suscripciones'}
             description={'Nuevas suscripciones en los últimos seis meses'}
-            redirect={`${url}/newSubscriptions`}/>
+            redirect={`${url}/newSubscriptions`}
+            click={true}
+          />
           <ReportItem
             image={activeUsers}
             title={'Usuarios'}
             description={'Usuarios más activos'}
-            redirect={`${url}/activeUsers`}/>
+            redirect={`${url}/activeUsers`}
+            click={true}
+          />
+          <ReportItem
+            image={activeUsers}
+            title={'Reproducciones semanales'}
+            description={'Total de reproducciones por semana'}
+            redirect={() => setModalShow(true)}
+            click={false}/>
+          <RangeDate
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+          <ReportItem
+            image={activeUsers}
+            title={'Artistas más escuchados'}
+            description={'N artistas con las mayores reproducciones'}
+            redirect={() => setModal1Show(true)}
+            click={false}
+          />
+          <ModalArtist
+            show={modal1Show}
+            onHide={() => setModal1Show(false)}
+          />
+          <ReportItem
+            image={activeUsers}
+            title={'Géneros más escuchados'}
+            description={'Total de reproducciones por género'}
+            redirect={() => setModal2Show(true)}
+            click={false}
+          />
+          <RangeDate
+            show={modal2Show}
+            onHide={() => setModal2Show(false)}
+          />
+          <ReportItem
+            image={activeUsers}
+            title={'Canciones más escuchadas'}
+            description={'Canciones más escuchadas '}
+            redirect={() => setModal3Show(true)}
+            click={false}
+          />
+          <ModalSongsArtists
+            show={modal3Show}
+            onHide={() => setModal3Show(false)}
+          />
         </div>
       </div>
+      {/* Presentación de reportes */}
       <section className={'section-container home-container'}>
             <div id="songs">
               <Switch>
@@ -77,7 +140,7 @@ export default function Report(){
                 <Route  path={`${path}/activeUsers`}>
                   <UsersReport/>
                 </Route>
-                <Route  path={path}/>
+                <Route  path={`${path}/report`} />
               </Switch>
             </div>
           </section>
---
+++ frontend/app/src/components/report/reports/modalArtist.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { useRouteMatch } from 'react-router-dom';
+import history from '../../history';
+import Axios from 'axios';
+
+export default function ModalArtist(props) {
+
+  const post = 'http://3.135.234.254:3000/createPlaylist' //Cambiar por la generación del reporte
+  let { url } = useRouteMatch();
+
+  //State variables
+  const [data, setData] = React.useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    limit: 0,
+  })
+  const [filled, setFilled] = React.useState({
+    startDate: false,
+    endDate: false,
+    limit: false,
+  });
+
+  function generateReport(){
+    const fetchData = async () => {
+      try {
+        const { data } = await Axios.post(post,
+          {
+            date: reportDate
+          }
+        );
+        console.log("todo fine")
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  };
+
+  const handleInputChange = (e) =>{
+    console.log(e.target.value);
+    setData({
+      ...data,
+      [e.target.name]:e.target.value,
+    })
+    if(e.target.value !== ''){
+      setFilled({
+        ...filled,
+        [e.target.name]: true
+      });
+    }else{
+      setFilled({
+        ...filled,
+        [e.target.name]: false
+      });
+    }
+  }
+
+  const handleClick = ()=>{
+    if(playlistName !== ''){
+      generateReport();
+      setTimeout(()=>{
+        console.log(history.location);
+        history.push(`${url}/songStreams`);
+        history.go();
+      },300)
+    }else{
+      alert('Indica un nombre para tu playlist')
+    }
+  }
+
+
+  return (
+    <Modal
+      {...props}
+      //size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+         Datos para reporte
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className={'position-relative mt-2'}>
+          <input className={"input " + (filled.startDate? 'is-filled':' ')}
+                 type="date"
+                 name="startDate"
+                 onChange={handleInputChange}
+          />
+          <label className={'label'}>Fecha de inicio</label>
+        </div>
+        <div className={'position-relative mt-2'}>
+          <input className={"input " + (filled.endDate? 'is-filled':' ')}
+                 type="date"
+                 name="endDate"
+                 onChange={handleInputChange}
+          />
+          <label className={'label'}>Fecha de fin</label>
+        </div>
+        <div className={'position-relative mt-2'}>
+          <input className={"input " + (filled.limit? 'is-filled':' ')}
+                 type="number"
+                 name="limit"
+                 onChange={handleInputChange}
+          />
+          <label className={'label'}>Límite de artistas</label>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button className={"border-btn mb-2"} onClick={props.onHide}>Cerrar</Button>
+        <Button className={'purple-btn mb-2'} >Generar reporte</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
