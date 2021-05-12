14d25a03134a754dd11150c3c88180e316d5f2ef
Zaray <cor191025@uvg.edu.gt>
2021-05-12 01:28:44 -0600

    arreglando query de getSpecificUser

--- database/src/controllers/usuarios.js
+++ database/src/controllers/usuarios.js
@@ -115,8 +115,8 @@ const getSpecificUser = async (req, res) => {
   const response = await pool.query(`
     SELECT concat(nombres, ' ' ,apellidos) as nombre, username, tu.descripcion as tipo, activo as estado
     FROM usuarios
-    INNER JOIN tipo_usuario tu on tu.id_tipousuario = usuarios.id_tipousuario
-    WHERE username ILIKE $1;`, [nombre]);
+        INNER JOIN tipo_usuario tu on tu.id_tipousuario = usuarios.id_tipousuario
+    WHERE username ILIKE $1 and (usuarios.id_tipousuario = 1 or usuarios.id_tipousuario = 2);`, [nombre]);
   res.status(200).json(response.rows);
 };
 
